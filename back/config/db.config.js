9feda11f51c04a83d7579e18383180a194399538
Gianmarco G <giangarof98@gmail.com>
2022-01-10 09:25:19 -0500

    dotenv added

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -2,9 +2,16 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 
-module.exports = {
-  host: "localhost",
-  user: "root",
-  password: "c+-jsgg6",
-  database: "p7"
+// module.exports = {
+//   host: "localhost",
+//   user: "root",
+//   password: "c+-jsgg6",
+//   database: "p7"
+// }
+
+module.exports ={
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
 }
